0fc13326b4b0a31717c62402d2b73488916954da
LevonnPapikyan <llevonpapikyan@gmail.com>
2025-06-18 20:42:34 +0400

    latest

--- WebViewerV2/settings.py
+++ WebViewerV2/settings.py
@@ -41,8 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = "arijgqoejrtgq049ut0q34u0t9afgjdlgisj4534"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
